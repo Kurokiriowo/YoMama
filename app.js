c1f8223520f399f0f8601c3b59259de86d86824f
Jason Petteys <petteysjason@gmail.com>
2017-07-26 07:42:06 -0600

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ fs.readdir('./commands/', (err, files) => {
   log(`Loading a total of ${files.length} commands.`);
   files.forEach(f => {
     let props = require(`./commands/${f}`);
-    log(`Loading Command: ${props.help.name}. 👌`);
+    log(`Loading Command: ${props.help.name}. ðŸ‘Œ`);
     client.commands.set(props.help.name, props);
     props.conf.aliases.forEach(alias => {
       client.aliases.set(alias, props.help.name);
@@ -57,12 +57,12 @@ client.elevation = message => {
   /* This function should resolve to an ELEVATION level which
      is then sent to the command handler for verification*/
   let permlvl = 0;
-  let trainee_role = message.guild.roles.find('name', settings.traineerolename);
-  if (trainee_role && message.member.roles.has(trainee_role.id)) permlvl = 1;
+  let train_role = message.guild.roles.find('name', settings.trainrolename);
+  if (train_role && message.member.roles.has(train_role.id)) permlvl = 1;
   let mod_role = message.guild.roles.find('name', settings.modrolename);
   if (mod_role && message.member.roles.has(mod_role.id)) permlvl = 2;
-  let sensei_role = message.guild.roles.find('name', settings.senseirolename);
-  if (sensei_role && message.member.roles.has(sensei_role.id)) permlvl = 3;
+  let trialadmin_role = message.guild.roles.find('name', settings.trialadminrolename);
+  if (trialadmin_role && message.member.roles.has(trialadmin_role.id)) permlvl = 3;
   let admin_role = message.guild.roles.find('name', settings.adminrolename);
   if (admin_role && message.member.roles.has(admin_role.id)) permlvl = 4;
   if (message.author.id === settings.ownerid) permlvl = 5;
