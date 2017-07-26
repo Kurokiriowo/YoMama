8d280f0d7d126b2deb8920b1b19327ffb6ea8c9b
Jason Petteys <petteysjason@gmail.com>
2017-07-26 07:33:22 -0600

    Updated ban.js
    Updated the DM message

--- commands/ban.js
+++ commands/ban.js
@@ -8,9 +8,9 @@ exports.run = (client, message, args) => {
   if (message.mentions.users.size < 1) return message.reply('You must mention someone to ban them.').catch(console.error);
 
   if (!message.guild.member(user).bannable) return message.reply('I cannot ban that member');
-  user.send(`You have been banned from The Social Annex, ${reason}`).then(member => {
+  user.send(`You have been banned from YoMamma by ${message.author.username}#${message.author.discriminator} for ${reason}. If you feel that this ban was in error or if you would like to appeal this ban, please join our appeal server, https://discord.gg/xCJJ4Gm`).then(member => {
                 message.guild.ban(user, {'days':1, 'reason':reason})
-                message.channel.sendMessage(`${user}, **Know Your Place**`).catch(console.error)
+                message.channel.sendMessage(`${user} **has been banned**`).catch(console.error)
               });
 
   const embed = new Discord.RichEmbed()
@@ -18,7 +18,7 @@ exports.run = (client, message, args) => {
     .setTimestamp()
     .setAuthor(`${message.author.username}#${message.author.discriminator}`, `${message.author.avatarURL}`)
     .setDescription(`**User:** ${user} -- ${user.username}#${user.discriminator} (${user.id})\n**Action:** Ban\n**Prune:** 1 day\n**Reason:** ${reason}`)
-    .setFooter('Ban','https://images.discordapp.net/avatars/310929237882437633/7640635d9bd12b9aa9729e1266e44561.png?size=1024')
+    .setFooter('Ban','https://images.discordapp.net/avatars/318917523615514645/da4157938a6f91f0ca746a890d1eae16.png?size=1024')
   return client.channels.get(modlog.id).sendEmbed(embed);
 };
 
@@ -26,7 +26,7 @@ exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: [],
-  permLevel: 3
+  permLevel: 2
 };
 
 exports.help = {
